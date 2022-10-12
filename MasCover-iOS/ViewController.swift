0f7dba3eef2c74e9f0e368b18fe514ef0be107af
taha jadid <taha.jadid@inwi.ma>
2022-10-12 19:12:56 +0100

    Initial Commit

---
+++ MasCover-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MasCover-iOS
+//
+//  Created by taha_jadid on 12/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
