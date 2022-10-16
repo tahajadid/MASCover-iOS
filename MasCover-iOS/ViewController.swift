4bc4ce83bb96f6af4a7015ad6c641073070b77b8
taha jadid <tahajadid98@gmail.com>
2022-10-16 08:38:22 +0100

    Add Splash Screen & animation extensions

--- MasCover-iOS/ViewController.swift
+++ MasCover-iOS/ViewController.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mascoverUI: UIImageView!
+    
+    @IBOutlet weak var phoneUi: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        //self.mascoverUI.fadeIn()
+        //self.phoneUi.fadeIn()
+
+        let oldCenterFirst = mascoverUI.center
+        let newCenterFirst = CGPoint(x: oldCenterFirst.x, y: oldCenterFirst.y + 40)
+
+        UIView.animate(withDuration: 1, delay: 0, options: .curveLinear, animations: {
+            self.mascoverUI.center = newCenterFirst
+        }) { (success: Bool) in
+          print("Done moving image")
+          }
+        
+        
+        let oldCenterSecond = phoneUi.center
+        let newCenterSecond = CGPoint(x: oldCenterSecond.x, y: oldCenterSecond.y - 180)
+
+        UIView.animate(withDuration: 1, delay: 0, options: .curveLinear, animations: {
+            self.phoneUi.center = newCenterSecond
+        }) { (success: Bool) in
+          print("Done moving image")
+          }
+        
     }
 
 
---
+++ MasCover-iOS/extension/UIViewExtension.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewExtension.swift
+//  MasCover-iOS
+//
+//  Created by taha_jadid on 15/10/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+
+    func fadeIn(_ duration: TimeInterval = 3.5, delay: TimeInterval = 1.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
+            self.alpha = 1.0
+    }, completion: completion)  }
+
+    func fadeOut(_ duration: TimeInterval = 0.5, delay: TimeInterval = 1.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
+            self.alpha = 0.3
+    }, completion: completion)
+   }
+}
