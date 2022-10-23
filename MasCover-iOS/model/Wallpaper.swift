2560696871bd39cd554ff1ee561b0f74c5d1d10a
taha jadid <tahajadid98@gmail.com>
2022-10-23 14:22:05 +0100

    Add custom wallpaper cell

---
+++ MasCover-iOS/model/Wallpaper.swift
@@ -0,0 +1,17 @@
+//
+//  Wallpaper.swift
+//  MasCover-iOS
+//
+//  Created by taha_jadid on 23/10/2022.
+//
+
+import Foundation
+
+struct Wallpaper: Codable {
+    var idWallpaper: String?
+    var idCategorie: String?
+    var numberDislike: Int?
+    var numberLike: Int?
+    var numberDownload: Int?
+    var pathPoster: String?
+}
